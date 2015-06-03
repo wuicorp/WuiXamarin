4b4e70be886952a73e330ec44e22b9205ba15db7
mindrod <victor.rodriguez.felipe@gmail.com>
2015-06-04 00:25:30 +0200

    vehicles

--- WuiAlertsApp/Helpers/GeolocationHelper.cs
+++ WuiAlertsApp/Helpers/GeolocationHelper.cs
@@ -85,6 +85,71 @@ namespace WuiAlertsApp.Helpers
 			return CurrentPosition;
 		}
 
+        //		public static async Task<Pin> GetCurrentPosition ()
+        public static async Task<Pin> GetCurrentPosition(int timeout)
+        {
+            if (Geolocator == null)
+            {
+                Geolocator = DependencyService.Get<IGeolocator>();
+                //				Geolocator.PositionError += OnListeningError;
+                //				Geolocator.PositionChanged += OnPositionChanged;
+            }
+
+            _cancelSource = new CancellationTokenSource();
+
+            PositionStatus = String.Empty;
+            PositionLatitude = String.Empty;
+            PositionLongitude = String.Empty;
+
+            //			TaskScheduler scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            //			CancellationTokenSource cancelSource = new CancellationTokenSource();
+            //			await Geolocator.GetPositionAsync(1000, cancelSource.Token, false)
+            //				.ContinueWith(t =>
+            //					{
+            //						//IsBusy = false;
+            //						if (t.IsFaulted)
+            //						{
+            //							PositionStatus = ((GeolocationException)t.Exception.InnerException).Error.ToString();
+            //						}
+            //						else if (t.IsCanceled)
+            //						{
+            //							PositionStatus = "Canceled";
+            //						}
+            //						else
+            //						{
+            //							PositionStatus = t.Result.Timestamp.ToString("G");
+            //							CurrentPosition = new Xamarin.Forms.Maps.Position(t.Result.Latitude, t.Result.Longitude);
+            //						}
+            //					}, scheduler);
+            //IsBusy = true;
+            await
+            Geolocator.GetPositionAsync(timeout: 10000, cancelToken: _cancelSource.Token, includeHeading: true)
+                .ContinueWith(t =>
+                {
+                    //IsBusy = false;
+                    if (t.IsFaulted)
+                        PositionStatus = ((GeolocationException)t.Exception.InnerException).Error.ToString();
+                    else if (t.IsCanceled)
+                        PositionStatus = "Canceled";
+                    else
+                    {
+                        PositionStatus = t.Result.Timestamp.ToString("G");
+                        PositionLatitude = "La: " + t.Result.Latitude.ToString("N4");
+                        PositionLongitude = "Lo: " + t.Result.Longitude.ToString("N4");
+                        CurrentPosition = new Pin
+                        {
+                            Type = PinType.Place,
+                            Position = new Xamarin.Forms.Maps.Position(t.Result.Latitude, t.Result.Longitude),
+                            Label = "CurrentPosition",
+                            Address = ""
+                        };
+                    }
+
+                }, _scheduler);
+
+            return CurrentPosition;
+        }
+
 		/// <summary>
 		/// Handles the <see cref="E:ListeningError" /> event.
 		/// </summary>
